2b10ef7d06c8214ebb2ec5eb050d74e2514ff269
devpheng <pheng.develop@gmail.com>
2023-04-02 20:38:30 +0700

    Create layouts

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+    
+    @include('includes.header')
+
+    @yield('content')
+
+    @include('includes.footer')
+    
+</body>
+
+</html>
